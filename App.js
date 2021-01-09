b7ac5cc77067fcd10fb2d8fbe4b56f225443728a
Lisa Peña <37710253+Adventurenow23@users.noreply.github.com>
2021-01-09 17:44:33 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('Lisa');
+  const [age, setAge] = useState('40');
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter name:</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. Jane Doe' />
+
+
+      <Text>name: {name}, age: {age}</Text>
+
     </View>
   );
 }
@@ -18,4 +30,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  }
+
+
+
+
 });
